03b4293e54a8b0bd6341a5c0b756bbc265ec7600
Caio Cesar Mekacheski de Araújo <106687263+CaioMekacheski@users.noreply.github.com>
2023-03-01 11:08:05 -0300

    Create classe_Profissao.java

---
+++ classe_Profissao.java
@@ -0,0 +1,17 @@
+//Criado 01/03/2023 10:51
+
+package minhas.finanças;
+
+public class Profissao 
+{
+    //Atributos
+    private String nome;
+    private String descr;
+    
+    //Construtor
+    public Profissao(String nome, String descr)
+    {
+        this.nome = nome;
+        this.descr = descr;
+    }
+}
