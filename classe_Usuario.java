50a5f6013b9925b290b3e31d2942b3b540004df0
Caio Cesar Mekacheski de Araújo <106687263+CaioMekacheski@users.noreply.github.com>
2023-03-01 11:05:10 -0300

    Create classe_Usuario.java

---
+++ classe_Usuario.java
@@ -0,0 +1,17 @@
+//Criado 01/03/2023 10:47
+package minhas.finanças;
+
+public class Usuario 
+{
+    //Atributos
+    private String nome;
+    private String senha;
+    
+    //Construtor
+    public Usuario(String nome, String senha)
+    {
+        this.nome = nome;
+        this.senha = senha;
+    }
+    
+}
